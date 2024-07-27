9624a298f48c82db20bb12081ff636e9a62cbf2c
NguyenDinhTienITHUFI <dinhtien147123@gmail.com>
2024-07-27 07:08:00 +0700

    Initial commit

---
+++ src/main/java/com/tiennguyen/betamvc/BetamvcApplication.java
@@ -0,0 +1,13 @@
+package com.tiennguyen.betamvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetamvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetamvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiennguyen/betamvc/BetamvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiennguyen.betamvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetamvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
